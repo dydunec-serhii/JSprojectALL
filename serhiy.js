10465ec0908259ac77293ea206148f5213df2dae
Serhii Dydunets <sergijdudinec69@gmail.com>
2025-07-14 18:08:53 +0300

    add

--- serhiy.js
+++ serhiy.js
@@ -1,4 +1,3 @@
-
 const dino = document.getElementById('dino');
 const cactus = document.getElementById('cactus');
 document.addEventListener('click', function (event) {
@@ -20,41 +19,3 @@ var isAlive = setInterval (function (){
     // }
 },10)
 
-
-// document.addEventListener('keydown', function (event) {
-//     const activeEl = document.activeElement;
-//     const isInput = activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA';
-
-//     if (event.code === 'Space' && !isInput) {
-//       event.preventDefault();
-//       // твоя логіка
-//     }
-//   });
-// const dino = document.querySelector('.dino-gogle');
-// let get = 50;
-// dino.addEventListener('keydown', (event) => {
-//     if (event.code === space) {
-//         event.preventDefault();
-//         get += 20;
-//         dino.setAttribute('cx', get);
-//     }
-// }) 
-// const dino = document.getElementById('dino-gogle');
-// let jumping = false;
-
-// dino.addEventListener('keydown', function (event) {
-//     if (event.code === 'Space' && !jumping) {
-//         event.preventDefault();
-//         jumping = true;
-
-//         // Підстрибнути вгору (менше y)
-//         dino.setAttribute('y', '80');
-
-//         // Через 400мс повернутися вниз
-//         setTimeout(() => {
-//             dino.setAttribute('y', '130');
-//             jumping = false;
-//         }, 400);
-//     }
-// });
-// >>>>>>> 8c91da71063189ad624e0094ad9abfba99b2d4f1
